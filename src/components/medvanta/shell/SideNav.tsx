83cefe013474294dd0d6d367e9c3677f44faeae6
Bruno <bruno@flywheel.so>
2026-09-18 13:43:59 -0300

    chore: capitalized vantavybe

--- src/components/medvanta/shell/SideNav.tsx
+++ src/components/medvanta/shell/SideNav.tsx
@@ -108,7 +108,7 @@ export function SideNav(): React.ReactElement {
             style={{ filter: 'brightness(0) invert(1)' }}
             priority
           />
-          <span className="side-pl">VantaVybe admin</span>
+          <span className="side-pl">VantaVybe Admin</span>
         </div>
       </div>
 
